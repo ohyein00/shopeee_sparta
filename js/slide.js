afb60a049008a46714cb7d90ecb15028cb217d20
yein <gpg7913@gmail.com>
2022-08-07 02:36:42 +0900

    (Create) category slide

--- js/slide.js
+++ js/slide.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('#topSlider', {
+const topSlider = new Swiper('#topSlider', {
   // Optional parameters
   loop: true,
   // If we need pagination
@@ -14,9 +14,22 @@ const swiper = new Swiper('#topSlider', {
     nextEl: '#topSlider .nav_next',
     prevEl: '#topSlider .nav_prev',
   },
+});
 
-
+const categorySlider = new Swiper('#categorySlider .category_slider', {
+  slidesPerView: 10,
+  slidesPerGroup :10,
+  direction: 'horizontal',
+  grid:{
+    fill:'row',
+    rows:2
+  },
+  navigation: {
+    nextEl: '#categorySlider .nav_next',
+    prevEl: '#categorySlider .nav_prev',
+  },
 });
 $(function(){
-  swiper()
+  topSlider()
+  categorySlider()
 })
