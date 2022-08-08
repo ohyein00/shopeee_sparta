c05656e1239061febb543ea17bfe454aa016b4fa
yein <gpg7913@gmail.com>
2022-08-08 11:24:44 +0900

    (Create) multislide

--- js/slide.js
+++ js/slide.js
@@ -29,3 +29,34 @@ const categorySlider = new Swiper('#categorySlider .category_slider', {
     prevEl: '#categorySlider .nav_prev',
   },
 });
+const mallFullSlider = new Swiper('#mallFullSlider', {
+  // Optional parameters
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.pagination',
+    type: 'bullets',
+  },
+  autoplay: {
+    delay: 5000, //슬라이드 settimeout간격
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '#mallFullSlider .nav_next',
+    prevEl: '#mallFullSlider .nav_prev',
+  },
+});
+
+const mallCateSlider = new Swiper('#mallCateSlider .category_slider', {
+  slidesPerView: 4,
+  slidesPerGroup :4,
+  direction: 'horizontal',
+  grid:{
+    fill:'row',
+    rows:2
+  },
+  navigation: {
+    nextEl: '#mallCateSlider .nav_next',
+    prevEl: '#mallCateSlider .nav_prev',
+  },
+});
